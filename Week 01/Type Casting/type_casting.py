a839c3e3ad8f85f33ce9b58c3bfb592905ec0dc6
azharaliofficial <azharali.officail.pk@gmail.com>
2026-04-25 17:55:48 +0500

    Week 01: Conditional Statement

--- Week 01/Type Casting/type_casting.py
+++
@@ -1,9 +0,0 @@
-age = "34"
-print(age)
-
-print(type(age))
-
-print(int(age)+1)
-
-input("write your age")
- 
--- Week 01/functions/local_global_func.py
+++
@@ -1,7 +0,0 @@
-def show():
-    x = 5 #local function
-    print(x)
-x=10 #global function
-show()
-
-print(x)
