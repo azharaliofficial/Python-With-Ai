3552868d082573c32d61c9300265d04e65c0e470
azharaliofficial <azharali.officail.pk@gmail.com>
2026-04-25 17:57:48 +0500

    Week 01: Operation

---
+++ Week 01/Operation/operation.py
@@ -0,0 +1,32 @@
+print(5+3) #Addition
+print(8-9) #Subtraction
+print(2*8) #Multiplication
+print(16/3) #Divide
+print(20//3) #floor Divide means point will not be shown
+print(11%3)  #reminder
+print(2**5)   #exponent
+print(5==5)  #equality
+print(7 != 7) #Inquality
+print(10>2) #Greater than
+print(2<10) #less than
+
+print(True and False) #and operator
+print(True or False) #or operator
+print(not True) #not operation
+
+x = 10 
+print(x)
+
+x +=5
+print(x)
+
+x -=7
+print(x)
+
+x *=2
+print(x)
+
+x /=10
+print(x)
+
+ 
