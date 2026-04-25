96145a4260c6e5d1345b1b33cf7fcbbd6bd84b0c
azharaliofficial <azharali.officail.pk@gmail.com>
2026-04-25 17:56:54 +0500

    Week 01: Functions

---
+++ Week 01/functions/local_global_func.py
@@ -0,0 +1,7 @@
+def show():
+    x = 5 #local function
+    print(x)
+x=10 #global function
+show()
+
+print(x)
---
+++ Week 01/functions/para_return_val.py
@@ -0,0 +1,9 @@
+def add(a,b):
+    return a+b
+result = add(2,3)
+print(result)
+
+def square(num):
+    return num*num
+result = square(4)
+print(result)
