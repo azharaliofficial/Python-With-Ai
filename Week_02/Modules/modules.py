000ddb579414e51153e3df39bc4e5b2cc00a85cb
azharaliofficial <azharali.official.pk@gmail.com>
2026-05-04 21:14:36 +0500

    Add Modules examples and usage

---
+++ Week_02/Modules/modules.py
@@ -0,0 +1,9 @@
+import math
+print(math.sqrt(100))
+
+from math import sqrt
+print(sqrt(81))
+
+import my_module
+my_module.display()
+my_module.calculate()
---
+++ Week_02/Modules/my_module.py
@@ -0,0 +1,8 @@
+def display():
+     print("hello world")
+
+def calculate():
+     a = 5
+     b = 5
+     sum = a + b
+     print(f"The result is : {sum}")
